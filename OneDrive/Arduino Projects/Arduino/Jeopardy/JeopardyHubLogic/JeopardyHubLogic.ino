140da4c233aef388bd89dfc1fe471ee5697c5d07
poatoak <kostoach000@gmail.com>
2026-07-11 16:53:44 -0500

    Added serial logic to control the game by pluggin the hub into a computer

--- OneDrive/Arduino Projects/Arduino/Jeopardy/JeopardyHubLogic/JeopardyHubLogic.ino
+++ OneDrive/Arduino Projects/Arduino/Jeopardy/JeopardyHubLogic/JeopardyHubLogic.ino
@@ -16,6 +16,7 @@ void onReceive(const uint8_t *mac, const uint8_t *data, int len) {
   memcpy(&msg, data, sizeof(msg));
   int ID = msg.ID;
 
+  Serial.println("BUZZ " + String(ID));
   if (!esp_now_is_peer_exist(mac)) {
     esp_now_peer_info_t peer = {};
     memcpy(peer.peer_addr, mac, 6);
@@ -27,6 +28,28 @@ void onReceive(const uint8_t *mac, const uint8_t *data, int len) {
   esp_now_send((uint8_t *)mac, (uint8_t *)&response, sizeof(response));
 }
 
-void setup() {}
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  esp_now_init();
+  esp_now_register_recv_cb(onReceive);
+}
 
-void loop() {}
+void loop() {
+  if (Serial.available()) {
+    String cmd = Serial.readStringUntil('\n');
+    cmd.trim();
+
+    if (cmd.startsWith("LOCK ")) {
+      int ID = cmd.substring(5).toInt();
+      lockouts[ID] = true;
+    } else if (cmd.startsWith("UNLOCK ")) {
+      int ID = cmd.substring(7).toInt();
+      lockouts[ID] = false;
+    } else if (cmd == "RESET") {
+      for (int i = 0; i < 8; i++) {
+        lockouts[i] = false;
+      }
+    }
+  }
+}
