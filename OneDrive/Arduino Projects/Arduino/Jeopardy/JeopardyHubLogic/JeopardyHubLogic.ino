9d0f62e567d5efd57de8a6bc2e15a03f607fb796
poatoak <kostoach000@gmail.com>
2026-07-11 16:10:44 -0500

    Cleaned up logic by Claude

--- OneDrive/Arduino Projects/Arduino/Jeopardy/JeopardyHubLogic/JeopardyHubLogic.ino
+++ OneDrive/Arduino Projects/Arduino/Jeopardy/JeopardyHubLogic/JeopardyHubLogic.ino
@@ -1,8 +1,7 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-bool[] lockouts = {false, false, false, false, false, false, false, false};
-uint8_t[] addresses {{}, {}, {}, {}, {}, {}, {}, {}};
+bool lockouts[] = {false, false, false, false, false, false, false, false};
 
 typedef struct Message { 
   int ID;
@@ -17,9 +16,9 @@ void onReceive(const uint8_t *mac, const uint8_t *data, int len) {
   memcpy(&msg, data, sizeof(msg));
   int ID = msg.ID;
 
-  esp_now_peer_info_t peer = {};
-  memcpy(peer.peer_addr, addresses[ID], 6);
-  if (!esp_now_is_peer_exist(addresses[ID])) {
+  if (!esp_now_is_peer_exist(mac)) {
+    esp_now_peer_info_t peer = {};
+    memcpy(peer.peer_addr, mac, 6);
     esp_now_add_peer(&peer);
   }
 
@@ -28,10 +27,6 @@ void onReceive(const uint8_t *mac, const uint8_t *data, int len) {
   esp_now_send((uint8_t *)mac, (uint8_t *)&response, sizeof(response));
 }
 
-void setup() {
+void setup() {}
 
-}
-
-void loop() {
-  
-}
+void loop() {}
